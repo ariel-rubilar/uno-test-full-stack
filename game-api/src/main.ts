037eeb30d442ca8da23062a8584d5eb656a4dfee
Ariel Rubilar <ariel.rubilar2@gmail.com>
2026-02-17 13:19:43 -0300

    chore: add docker for api

--- game-api/src/main.ts
+++ game-api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 3001;
   console.log(`Starting server on port ${port}...`);
   await app.listen(port);
 }
